907dcc895c60bf67542341aa40acbc24f3b2e6dd
LuisDani07 <tics270118@gmail.com>
2022-10-30 17:28:14 -0700

    configuración del service worker

---
+++ public/serviceworker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME="version-1";
+const urlsToCache=['index.html', 'offline.html'];
+
+
+const self=this;
+//install sw
+self.addEventListener('install', (event)=>{
+event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache)=>{
+        console.log('open cache');
+        return cache.addAll(urlsToCache);
+    })
+)
+});
+
+//listen for requests
+self.addEventListener('fetch', (event)=>{
+    
+});
+
+//activate sw
+self.addEventListener('activate', (event)=>{
+    
+});
